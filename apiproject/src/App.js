7c7d17a3adbb785bf17357c863e704ac4022be6d
Dennis Manyagi <manyagi.dennis@gmail.com>
2019-05-10 22:10:09 -0700

    starter project api

---
+++ apiproject/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoaded: true,
+            items: json,
+          })
+        })
+  }
+
+  render() {
+
+
+    let { isLoaded, items } = this.state;
+
+    if(!isLoaded){
+      return <div> Loading...</div>;
+    }
+    else {
+
+
+      return (
+          <div className="App">
+            <ul>
+              {items.map(item =>(
+                  <li key={item.id}>
+                    NAME: {item.name} || EMAIL: {item.email}
+                  </li>
+              ))};
+            </ul>
+
+          </div>
+      );
+    }
+}
+}
+
+export default App;

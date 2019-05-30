d9e805d4da898c7048dac7de30009bcdeddcd8a9
Dennis Manyagi <manyagi.dennis@gmail.com>
2019-05-29 18:48:46 -0700

    Updates

---
+++ precache-manifest.8eb8e503e8191138e4e1803885be6718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743392a6db9197e4253c78aa25db5c9c",
+    "url": "/ApiProject/index.html"
+  },
+  {
+    "revision": "6d1942fa09e5d8aa8428",
+    "url": "/ApiProject/static/css/main.aa16945d.chunk.css"
+  },
+  {
+    "revision": "18e321e5481b75d98162",
+    "url": "/ApiProject/static/js/2.a39d37e3.chunk.js"
+  },
+  {
+    "revision": "6d1942fa09e5d8aa8428",
+    "url": "/ApiProject/static/js/main.35665164.chunk.js"
+  },
+  {
+    "revision": "ad87bc6974e0a4f3fc15",
+    "url": "/ApiProject/static/js/runtime~main.3f57f3d8.js"
+  }
+]);
